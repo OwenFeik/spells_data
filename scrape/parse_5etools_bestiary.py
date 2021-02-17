27fafdb4b47a2f70dd4fbaa1a8e858bf57e1e4bc
OwenFeik <owenhfeik@gmail.com>
2021-02-17 16:46:35 +1100

    update bestiary

--- scrape/parse_5etools_bestiary.py
+++ scrape/parse_5etools_bestiary.py
@@ -3,16 +3,17 @@ import re
 
 ZERO_WIDTH_SPACE = "\u200b"
 
-with open('temp.json', 'r') as f:
-    creatures = json.load(f)
-
 def sub_tags(string):
     string = string \
         .replace("{@atk mw}", "Melee weapon attack") \
         .replace("{@atk rw}", "Ranged weapon attack") \
         .replace("{@atk mw,rw}", "Melee or ranged weapon attack") \
+        .replace("{@atk ms,rs}", "Melee or ranged spell attack") \
         .replace("{@h}", "On hit: ") \
-        .replace("{@recharge}", "(recharge on 6)")
+        .replace("{@recharge}", "(recharge on 6)") \
+        .replace("rs {@hitYourSpellAttack}", "Ranged spell attack: your spell attack modifier") \
+        .replace("ms {@hitYourSpellAttack}", "Melee spell attack: your spell attack modifier") \
+        .replace("{@hitYourSpellAttack}", "your spell attack modifier")
     # {@hit n} -> +n
     string = re.sub(
         r'{@hit (\d+)}',
@@ -34,9 +35,16 @@ def sub_tags(string):
         string,
         flags=re.MULTILINE
     )
+    # {@creature in text name|set} -> in text name
+    string = re.sub(
+        r'{@creature ([\w\(\)\, ]*)\|\w+}',
+        r'\1',
+        string,
+        flags=re.MULTILINE
+    )
     # {@creature 5e.tools name||in text name} -> in text name
     string = re.sub(
-        r'{@creature [\w ]*\|[\w ]*\|(\w+)}',
+        r'{@creature [\w\(\)\, ]*\|[\w\(\)\, ]*\|([\w\,\(\) ]+)}',
         r'\1',
         string,
         flags=re.MULTILINE
@@ -48,14 +56,66 @@ def sub_tags(string):
         string,
         flags=re.MULTILINE
     )
+    # {@item name|set} -> name
+    string = re.sub(
+        r"{@item ([\w'\+\-\(\) ]+)\|[\w'\+\-\(\) ]*}",
+        r'\1',
+        string,
+        flags=re.MULTILINE
+    )
+    # {@item name|set|text} -> text
+    string = re.sub(
+        r"{@item [\w'\+\-\(\)\, ]+\|[\w'\+\-\(\)\, ]*\|([\w'\+\-\(\)\, ]*)}",
+        r'\1',
+        string,
+        flags=re.MULTILINE
+    )
+    # {@table name|set} -> name table
+    string = re.sub(
+        r'{@table ([^\{\}\|]+)(\|(phb|GoS|DMG))?}',
+        r'\1 table',
+        string,
+        flags=re.MULTILINE
+    )
+    # {@filter in text name|location|type=name} -> in text name
+    string = re.sub(
+        r"{@filter ([\w'\+\-\(\) ]+)\|[\w'\+\-\(\) ]*\|[\w'\+\-\(\)\= ]*}",
+        r'\1',
+        string,
+        flags=re.MULTILINE
+    )
+    # {@spell link name||in text name} -> in text name
+    string = re.sub(
+        r'{@spell [^\{\}\|]+\|[^\{\}\|]*(\|[^\{\}\|]*)?}',
+        r'\1',
+        string,
+        flags=re.MULTILINE
+    )
+    # {@book in text name|book|chapter|title} -> in text name
+    string = re.sub(
+        r"{@book ([\w'\+\-\(\) ]+)\|[\w'\+\-\(\) ]*\|[\w'\+\-\(\)\= ]*\|[\w'\+\-\(\)\= ]*}",
+        r'\1',
+        string,
+        flags=re.MULTILINE
+    )
     # {@x y} -> y
     string = re.sub(
-        r'{@\w+ ([\w\s\+\-\u00d7\. ]+)(\|phb)?}',
+        r'{@\w+ ([^\{\}\|]+)(\|(phb|GoS|DMG))?}',
         r'\1',
         string,
         flags=re.MULTILINE
     )
     return string
+
+def get_size(c):
+    return {
+        "T": "Tiny",
+        "S": "Small",
+        "M": "Medium",
+        "L": "Large",
+        "H": "Huge",
+        "G": "Gargantuan"
+    }[c["size"]]
 
 def get_ac(c):
     text = ""
@@ -76,29 +136,56 @@ def get_ac(c):
             text += " (" + ", ".join(sub_tags(s) for s in ac["from"]) + ")"
     return text
 
-def get_aligment(c):
+def get_hp(hp):
+    if "average" in hp and "formula" in hp:
+        return str(hp["average"]) + " (" + hp["formula"] + ")"
+    else:
+        return hp["special"]
+
+def get_alignment(c):
+    if not "alignment" in c:
+        return ""
     if type(c["alignment"][0]) is str:
         return "".join(c["alignment"])
     else:
-        return " or ".join("".join(a["alignment"]) + \
-            " (" + str(a["chance"]) + "%)" for a in c["alignment"])
+        entries = []
+        for a in c["alignment"]:
+            if "chance" in a:
+                entries.append(
+                    "".join(a["alignment"]) + "(" + str(a["chance"]) + "%)"
+                )
+            elif "special" in a:
+                entries.append(a["special"])
+            else:
+                entries.append("".join(a["alignment"]))
+
+        return " or ".join(entries)
 
 def parse_traits(l):
     traits = []
     for t in l:
-        trait = {"name": sub_tags(t["name"])}
+        trait = {"name": sub_tags(t["name"]) if "name" in t else ZERO_WIDTH_SPACE}
         text = ""
         for e in t["entries"]:
             if type(e) is str: # single line of text
                 text += sub_tags(e) + "\n\n"
+            elif "entries" in e and "type" in e and e["type"] == "inline":
+                for i in e["entries"]:
+                    if type(i) is str:
+                        text += sub_tags(i)
+                    else:
+                        text += sub_tags(i["text"])
             else: # list of items
                 for i in e["items"]:
-                    if "entry" in i:
+                    if type(i) is str:
+                        text += f"\u2022 {sub_tags(i)}\n"
+                    elif "entry" in i:
                         text += f"\u2022 {sub_tags(i['name'])}" \
                             f" {sub_tags(i['entry'])}\n"
                     else:
                         entry = sub_tags('\n\t'.join(i["entries"]))
                         text += f"\u2022 {sub_tags(i['name'])} {entry}\n"
+
         trait["text"] = text[:-2]
         traits.append(trait)
     return traits
@@ -130,46 +217,60 @@ def parse_legendary_actions(c):
 
 def parse_speed(speed):
     entries = []
-    for s in speed:
-        if type(speed[s]) is int:
-            entries.append(f'{s} {speed[s]} ft.')
-            continue
-        if type(speed[s]) is bool:
-            continue
-        entries.append(f"{s} {speed[s]['number']} ft. {speed[s]['condition']}")
+    try:
+        for s in speed:
+            if type(speed[s]) is int:
+                entries.append(f'{s} {speed[s]} ft.')
+            elif type(speed[s]) is bool:
+                continue
+            elif s == "choose":
+                entries.append(f"choose from {', '.join(speed[s]['from'])} " \
+                    f"{speed[s]['amount']} ft. {speed[s]['note']}")
+            else:
+                entries.append(f"{s} {speed[s]['number']} ft. {speed[s]['condition']}")
+    except:
+        print(speed)
     return ", ".join(entries)
 
-out = []
-for c in creatures:
-    d = {k: c.get(k) for k in
-        [
-            "name",
-            "size",
-            "type",
-            "str",
-            "dex",
-            "con",
-            "int",
-            "wis",
-            "cha",
-            "languages",
-            "cr",
-            "speed",
-            "save",
-            "skill",
-            "senses"
-        ]
-    }
+def parse_json(creatures):
+    out = []
+    for c in creatures:
+        if "_copy" in c:
+            continue
 
-    d["ac"] = get_ac(c)
-    d["hp"] = str(c["hp"]["average"]) + " (" + c["hp"]["formula"] + ")"
-    d["alignment"] = get_aligment(c)
-    d["speed"] = parse_speed(c.get("speed", {}))
-    d["traits"] = parse_traits(c.get("trait", []))    
-    d["actions"] = parse_traits(c.get("action", []))
-    d["legendary_actions"] = parse_legendary_actions(c)
+        d = {k: c.get(k) for k in
+            [
+                "name",
+                "size",
+                "type",
+                "str",
+                "dex",
+                "con",
+                "int",
+                "wis",
+                "cha",
+                "languages",
+                "cr",
+                "speed",
+                "save",
+                "skill",
+                "senses"
+            ]
+        }
 
-    out.append(d)
+        d["saves"] = d["save"]
+        del d["save"]
+        d["skills"] = d["skill"]
+        del d["skill"]
 
-with open('out.json', 'w') as f:
-    json.dump(out, f, indent=4)
+        d["size"] = get_size(c)
+        d["ac"] = get_ac(c)
+        d["hp"] = get_hp(c["hp"])
+        d["alignment"] = get_alignment(c)
+        d["speed"] = parse_speed(c.get("speed", {}))
+        d["traits"] = parse_traits(c.get("trait", []))    
+        d["actions"] = parse_traits(c.get("action", []))
+        d["legendary_actions"] = parse_legendary_actions(c)
+
+        out.append(d)
+    return out
---
+++ scrape/scrape_5etools_bestiary.py
@@ -0,0 +1,21 @@
+import json
+
+import requests
+
+import parse_5etools_bestiary
+
+ROOT_URL = "https://5e.tools/data/bestiary/"
+VERSION = "?v=1.122.8"
+INDEX_URL = ROOT_URL + "index.json" + VERSION
+
+out = []
+for f in requests.get(INDEX_URL).json().values():
+    try:
+        data = requests.get(ROOT_URL + f + VERSION).json()["monster"]
+    except:
+        print(f)
+
+    out.extend(parse_5etools_bestiary.parse_json(data))
+
+with open("out.json", "w") as f:
+    json.dump(out, f, indent=4)
